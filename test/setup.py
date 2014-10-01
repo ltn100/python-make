30c219ac409d12a6a3bc960fa7c42c7cccb8e333
ltn100 <ltn100@gmail.com>
2014-10-01 14:54:27 +0000

    Initial commit

---
+++ test/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# vim:set softtabstop=4 shiftwidth=4 tabstop=4 expandtab:
+
+from distutils.core import setup
+
+
+setup(name='test-python-make',
+    # Must comply with http://legacy.python.org/dev/peps/pep-0440/#version-scheme
+    version='0.1',
+
+    description='A test package',
+
+    url='https://github.com/ltn100/python-make',
+    license='MIT licence, see LICENCE',
+
+    packages=['test-python-make'],
+
+)
+
